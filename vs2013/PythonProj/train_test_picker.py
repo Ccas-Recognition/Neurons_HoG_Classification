6b73e2ab37588dd854be26423609e26f1b8b45db
alexey <shadelingen@gmail.com>
2014-11-10 19:12:28 +0300

    Change python script, bmp files is absolute

--- vs2013/PythonProj/train_test_picker.py
+++ vs2013/PythonProj/train_test_picker.py
@@ -13,11 +13,12 @@ else:
     sampling_count = 1000
     training_count = 500
 
-training_data_dir_txt = 'training_data/'
+training_data_dir_txt = 'total_data/'
 training_data_dir = '../../data/neuron/training_data/'
-total_data_dir = '../../data/neuron/totall_data/'
+total_data_dir = '../../data/neuron/total_data/'
 
-def bmpToJpg(mypath, label):
+def bmpToJpg(subfolder, label):
+    mypath = total_data_dir + subfolder
     onlyfiles = np.array([ f for f in listdir(mypath) if isfile(join(mypath,f)) and splitext(f)[1] == ".jpg"  ])
 
     indx = np.random.choice(onlyfiles.size, min( sampling_count, len( onlyfiles) ), replace = False)
@@ -26,27 +27,18 @@ def bmpToJpg(mypath, label):
         fileName, fileExtension = splitext(file)
         if(fileExtension != '.jpg'):
             continue
-        ftrain.write(training_data_dir_txt + fileName + '.bmp ' + str(label) + '\n')
+        ftrain.write(training_data_dir_txt + subfolder + file + ' ' + str(label) + '\n')
         #print([label, fileName])
-        new_name = training_data_dir + fileName + '.bmp'
-        if(not isfile(new_name) ):
-            img = cv2.imread(mypath + file)
-            cv2.imwrite(new_name, img)
-        #else:
-        #    print('it is ok: ' + new_name)
 
     for file in onlyfiles[indx[training_count:]]:
         fileName, fileExtension = splitext(file)
         if(fileExtension != '.jpg'):
             continue
-        ftest.write(training_data_dir_txt + fileName + '.bmp ' + str(label) + '\n')
+        ftest.write(training_data_dir_txt + subfolder + file + ' ' + str(label) + '\n')
         #print([label, fileName])
-        new_name = training_data_dir + fileName + '.bmp'
-        if(not isfile(new_name) ):
-            img = cv2.imread(mypath + file)
-            cv2.imwrite(training_data_dir + fileName + '.bmp', img)
 
-bmpToJpg(total_data_dir + 'bg/', 1)
-bmpToJpg(total_data_dir + 'fg/', 2)
+
+bmpToJpg( 'bg/', 1)
+bmpToJpg( 'fg/', 2)
 
 print('Sampling generated: %d training, %d testing'%((training_count*2), ((sampling_count - training_count)*2)))
