18bb4996e2285563381beae735b53e7de82a2034
alexey <shadelingen@gmail.com>
2014-11-16 14:51:37 +0300

    Disable post build events

--- vs2013/lib_post_build_event.bat
+++ vs2013/lib_post_build_event.bat
@@ -4,7 +4,7 @@ If Exist ..\..\repo\neuronsrecognition\hog\ (
 ::copy src\classifier.h ..\..\repo\neuronsrecognition\hog\classifier.h
 ::copy vs2013\liblinear\linear.h ..\..\repo\neuronsrecognition\hog\linear.h
 ::copy vs2013\liblinear\tron.h ..\..\repo\neuronsrecognition\hog\tron.h
-copy src\HOG_Functor.h ..\..\repo\neuronsrecognition\hog\HOG_Functor.h
-copy src\consts.h ..\..\repo\neuronsrecognition\hog\consts.h
-copy vs2013\Release\Neurons_HoG_Classification_Lib.lib ..\..\repo\neuronsrecognition\hog\hog.lib
+::copy src\HOG_Functor.h ..\..\repo\neuronsrecognition\hog\HOG_Functor.h
+::copy src\consts.h ..\..\repo\neuronsrecognition\hog\consts.h
+::copy vs2013\Release\Neurons_HoG_Classification_Lib.lib ..\..\repo\neuronsrecognition\hog\hog.lib
 )
--- vs2013/lib_post_build_event_debug.bat
+++ vs2013/lib_post_build_event_debug.bat
@@ -4,9 +4,9 @@ If Exist ..\..\repo\neuronsrecognition\hog\ (
 ::copy src\classifier.h ..\..\repo\neuronsrecognition\hog\classifier.h
 ::copy vs2013\liblinear\linear.h ..\..\repo\neuronsrecognition\hog\linear.h
 ::copy vs2013\liblinear\tron.h ..\..\repo\neuronsrecognition\hog\tron.h
-copy src\HOG_Functor.h ..\..\repo\neuronsrecognition\hog\HOG_Functor.h
-copy src\consts.h ..\..\repo\neuronsrecognition\hog\consts.h
-copy vs2013\Debug\Neurons_HoG_Classification_Lib.lib ..\..\repo\neuronsrecognition\hog\hogd.lib
+::copy src\HOG_Functor.h ..\..\repo\neuronsrecognition\hog\HOG_Functor.h
+::copy src\consts.h ..\..\repo\neuronsrecognition\hog\consts.h
+::copy vs2013\Debug\Neurons_HoG_Classification_Lib.lib ..\..\repo\neuronsrecognition\hog\hogd.lib
 
-copy model_binary.txt ..\..\repo\neuronsrecognition\hog\hog_model.txt
+::copy model_binary.txt ..\..\repo\neuronsrecognition\hog\hog_model.txt
 )
